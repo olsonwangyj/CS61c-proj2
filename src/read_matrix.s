8f3bfa47575d4f00df31e4f9f1d62cf975f339e5
Wang Yanjie <olsonwangyj@gmail.com>
2024-06-23 19:31:44 +0800

    finish partB task1

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -27,16 +27,85 @@
 read_matrix:
 
     # Prologue
-	
-
-
-
-
-
-
-
-
+	addi sp, sp, -28
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw s4, 16(sp)
+    sw s5, 20(sp)
+    sw ra, 24(sp)
+    mv s0, a0
+    mv s1, a1
+    mv s2, a2
+    
+    mv a1, s0
+    add a2, x0, x0
+    call fopen
+    li t0, -1
+    beq a0, t0, fopen_error
+   
+    mv s3, a0                  # s3 is the file descriptor
+    mv a1, s3
+    mv a2, s1
+    li a3, 4
+    call fread
+    bne a0, a3, fread_error
+    
+    mv a1, s3
+    mv a2, s2
+    li a3, 4
+    call fread
+    bne a0, a3, fread_error
+    
+    lw t0, 0(s1)
+    lw t1, 0(s2)
+    mul s4, t0, t1              # s4 is the row * col
+    li t2, 4
+    mul s4, t2, s4
+    mv a0, s4
+    call malloc
+    beq a0, x0, malloc_error
+    mv s5, a0                   # s5 is the array
+    mv a1, s3
+    mv a2, s5
+    mv a3, s4
+    call fread
+    bne a0, a3, fread_error
+    
+    mv a1, s3
+    call fclose
+    bne a0, x0, fclose_error
+    
+    mv a0, s5
+    mv a1, s1
+    mv a2, s2
+    
     # Epilogue
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp)
+    lw ra, 24(sp)
+    addi sp, sp, 28
 
     ret
+    
+fopen_error:
+    li a1, 90
+    j exit2
+
+fread_error:
+    li a1, 91
+    jal exit2
+    
+malloc_error:
+    li a1, 88
+    jal exit2
+    
+fclose_error:
+    li a1, 92
+    jal exit2
+    
